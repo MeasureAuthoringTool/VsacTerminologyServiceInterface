4b4be9e353f9aad7099294fb64b95e85a7e8e2f2
Gregory P. Akins <gregory.akins@semanticbits.com>
2021-08-11 15:28:44 -0400

    MAT-3246: Adding initial commit

---
+++ src/main/java/com/semanticbits/vsacterminologyserviceinterface/VsacterminologyserviceinterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.semanticbits.vsacterminologyserviceinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsacterminologyserviceinterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsacterminologyserviceinterfaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/semanticbits/vsacterminologyserviceinterface/VsacterminologyserviceinterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semanticbits.vsacterminologyserviceinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VsacterminologyserviceinterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
